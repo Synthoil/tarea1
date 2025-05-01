d2f85fe811d1c1e87acb74cf29827eaec2594102
Benjamin <benjamidiaz2023@udec.cl>
2025-05-01 02:04:23 -0400

    Excepciones agregadas a Main para su uso correcto

--- Tarea_Expendedor/src/main/java/Main.java
+++ Tarea_Expendedor/src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
         Expendedor exp = new Expendedor(7);
         Moneda m = null;
         Comprador c = null;
