0213afd4c6fb2c22c291b09614ee559cc238e7b8
EleMado <lmaldonado2024@udec.cl>
2025-05-01 14:56:17 -0400

    Implementacion excexeptions y prueba de los casos

--- Tarea_Expendedor/src/main/java/Main.java
+++ Tarea_Expendedor/src/main/java/Main.java
@@ -1,16 +1,51 @@
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
-        Expendedor exp = new Expendedor(7);
-        Moneda m = null;
-        Comprador c = null;
+    public static void main(String[] args) {
+        Expendedor exp = new Expendedor(1); // solo un producto de cada tipo
 
-        m = new Moneda1000();
-        c = new Comprador(m, Expendedor.SPRITE,exp);
-        System.out.println(c.queConsumiste());
-        System.out.println(c.cuantoVuelto());
+        // Monedas
+        Moneda m1000 = new Moneda1000();
+        Moneda m500 = new Moneda500();
+        Moneda m100 = new Moneda100();
 
+        // compra exitosa
+        try {
+            Comprador c1 = new Comprador(m1000, Expendedor.COCA, exp);
+            System.out.println("comprador 1 consumió: " + c1.queConsumiste());
+            System.out.println("Vuelto de comprador 1 :" + c1.cuantoVuelto());
+        }
+        catch (Exception e) {
+            System.out.println("Error en comprador 1: " + e.getMessage());
+        }
+
+        // Compra sin suficiente dinero
+        try {
+            Comprador c2 = new Comprador(m100, Expendedor.SUPER8, exp);
+            System.out.println("comprador 2 consumió: " + c2.queConsumiste());
+            System.out.println("Vuelto de comprador 2:" + c2.cuantoVuelto());
+        } catch (PagoInsuficienteException e) {
+            System.out.println("Pago insuficiente: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error en comprador 2: " + e.getMessage());
+        }
+
+        // Compra con moneda null
+        try {
+            Comprador c3 = new Comprador(null, Expendedor.FANTA, exp);
+        } catch (PagoIncorrectoException e) {
+            System.out.println("Pago incorrecto de comprador 3: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error en comprador 3: " + e.getMessage());
+        }
+
+        // Compra cuando no hay stock (ya se compró antes)
+        try {
+            Comprador c4 = new Comprador(m500, Expendedor.COCA, exp);
+        } catch (NoHayProductoException e) {
+            System.out.println("No hay producto en la compra 4: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error en comprador 4: " + e.getMessage());
+        }
     }
 }
-
